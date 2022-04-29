32293ac99526482c5c3ad969207da2c459b307e4
Pablo Castillo <pablocastillo.5.1992@gmail.com>
2022-04-29 14:37:26 -0500

    Se hizo el upgrade de versión de la dependencia para javax.servlet el cual permite declarar en la anotación el urlpatterns para no mappear en el web.xml los servlets, y se modificó el web.xml para poder gestionar correctamente las anotaciones de los servlets

--- src/main/java/com/pdev/Controller/ServletProductos.java
+++ src/main/java/com/pdev/Controller/ServletProductos.java
@@ -1,12 +1,14 @@
 package com.pdev.Controller;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet(name = "ServletProductos", urlPatterns = {"/productos"})
 public class ServletProductos extends HttpServlet {
 
     @Override
