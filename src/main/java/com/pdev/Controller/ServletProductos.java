3dad1502f60b98772f3b7f8839ef9f00726c301b
Pablo Castillo <pablocastillo.5.1992@gmail.com>
2022-04-28 20:18:04 -0500

    Creación Proyecto Web Ventas con contenedor Jetty, configuración de puerto e inicio de configuración servlets

---
+++ src/main/java/com/pdev/Controller/ServletProductos.java
@@ -0,0 +1,34 @@
+package com.pdev.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletProductos extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter out;
+        String title = "Simple Servlet Output";
+
+        // primero selecciona el tipo de contenidos y otros campos de cabecera de la respuesta
+        resp.setContentType("text/html");
+        // Luego escribe los datos de la respuesta
+        out = resp.getWriter();
+        out.println("<HTML><HEAD><TITLE>");
+        out.println(title);
+        out.println("</TITLE></HEAD><BODY>");
+        out.println("<H1>" + title + "</H1>");
+        out.println("<P>This is output from SimpleServlet!");
+        out.println("</BODY></HTML>");
+        out.close();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
